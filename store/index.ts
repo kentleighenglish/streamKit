268eeead33abf3308d5d524b996b638882fbe366
Kentleigh English <kentleigh@ikengainnovations.com>
2021-04-03 03:50:08 +0100

    Remove redundant mutations

--- store/index.ts
+++ store/index.ts
@@ -28,9 +28,7 @@ if (process.env.NODE_ENV === 'development') {
 export const plugins = middleware;
 
 export const state = () => ({
-  currentSet: null,
 	socket: null,
-	sets: [],
 	devices: []
 });
 
@@ -44,26 +42,8 @@ export const actions = {
 };
 
 export const mutations = {
-	[MUTATION_TYPES.OPEN_MODAL]: function(state, { modal }) {
-		Vue.set(state, "visibleModal", modal);
-	},
   [MUTATION_TYPES.ADD_SOCKET]: function(state, socket) {
     Vue.set(state, "socket", socket);
-  },
-  [MUTATION_TYPES.INIT_SET]: function(state) {
-    const currentSet = sessionStorage.getItem("currentSet");
-
-    if (currentSet) {
-      Vue.set(state, "currentSet", currentSet);
-    }
-  },
-  [MUTATION_TYPES.LOAD_SET]: function(state, set) {
-    Vue.set(state, "currentSet", set);
-
-    sessionStorage.setItem("currentSet", set);
-  },
-  [MUTATION_TYPES.UPDATE_SETS]: function(state, sets) {
-    Vue.set(state, "sets", sets);
   },
   [MUTATION_TYPES.UPDATE_DEVICES]: function(state, devices) {
     Vue.set(state, "devices", devices);
