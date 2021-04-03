8da65e95be826c7694b3a615fdf4e3568fe071fb
Kentleigh English <kentleigh@ikengainnovations.com>
2021-04-03 18:20:44 +0100

    Update editor route

---
+++ pages/editor.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container sk-flex sk-fill">
+    <div class="sk-flex sk-fill">
+      <div class="sk-flex sk-fill sk-column sk-padding-h">
+        <viewport class="sk-padding"></viewport>
+        <EditorTrackbar class="sk-padding" />
+      </div>
+      <div class="sk-flex sk-flex-30 sk-column sk-padding-h">
+        <EditorToolbar class="sk-padding" />
+      </div>
+    </div>
+  </div>
+</template>
