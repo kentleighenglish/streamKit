d1a2b8a083452cbeeec609555324c700eb2648b4
Kentleigh English <kentleigh@ikengainnovations.com>
2021-04-04 03:55:35 +0100

    Add more common components

---
+++ components/common/Button.vue
@@ -0,0 +1,114 @@
+<template>
+  <button
+    :type="type"
+    :class="{ skButton: true, 'skButton--loading': loading }"
+    :disabled="disabled"
+    @click="onClick($event)"
+  >
+    <div class="skButton__content">
+      <slot></slot>
+    </div>
+    <div class="skButton__frame">
+      <div class="skButton__frameInner" ng-if="hoverIcon">
+        <span ng-if="!loading" class="icon">{{ hoverIcon }}</span>
+        <Loading ng-if="loading" />
+      </div>
+    </div>
+  </button>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    type: {
+      type: String,
+      default: "button",
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    hoverIcon: {
+      type: String,
+      default: null,
+    },
+  },
+});
+</script>
+<style lang="scss">
+.skButton {
+  display: flex;
+  position: relative;
+  background: none;
+  padding: $gap ($gap * 2);
+  margin: $gap 0;
+  color: $primary;
+  font-weight: 300;
+
+  @include applyFontStandards();
+
+  cursor: pointer;
+  overflow: hidden;
+
+  border: none;
+
+  &:active,
+  &:focus {
+    outline: none;
+  }
+
+  &:disabled {
+    cursor: default;
+    color: $grey-dark;
+  }
+}
+
+.skButton__content {
+  ng-transclude {
+    display: flex;
+    align-items: center;
+  }
+}
+
+.skButton__frame {
+  z-index: 2;
+  position: absolute;
+  border: 2px solid $primary-dark;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  transform: translateY(100%);
+  pointer-events: none;
+  margin-top: -2px;
+
+  transition: border-color 0.1s, transform 0.2s, margin-top 0.2s;
+
+  .skButton:hover:not(:disabled) &,
+  .skButton--loading & {
+    border-color: $primary;
+    transform: translateY(0%);
+    margin-top: 0px;
+  }
+
+  .skButton:disabled & {
+    border-color: $grey-darker;
+    color: $grey;
+  }
+}
+
+.skButton__frameInner {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background: $grey-darkest;
+
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ components/common/Loading.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="loading">
+    <div class="loading__spinner"></div>
+    <div class="loading__smallSpinner"></div>
+  </div>
+</template>
+<style lang="scss">
+@keyframes loadingAnim {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(359deg);
+  }
+}
+
+.loading {
+  display: flex;
+  position: relative;
+  width: 1.5em;
+  vertical-align: middle;
+  justify-content: center;
+  align-items: center;
+
+  &:before {
+    display: block;
+    content: "";
+    padding-top: 100%;
+  }
+}
+
+.loading__spinner {
+  display: flex;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  border-radius: 50%;
+  border: 3px solid transparent;
+
+  border-left-color: $primary;
+  border-right-color: $primary;
+
+  animation: loadingAnim infinite 0.7s linear;
+}
+</style>
